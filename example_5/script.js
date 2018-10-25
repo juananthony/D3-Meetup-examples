2ad0d4cf5f9f4ab11c83209a4a30ff5b7aef80ed
Juan Antonio <ruchin10@gmail.com>
2018-10-25 12:44:56 +0200

    new examples

---
+++ example_5/script.js
@@ -0,0 +1,54 @@
+d3.json("../data/auto.json").then( data => {
+
+    // configuracion del svg
+    var config = {
+        height: 600,
+        width: 800
+    }
+    
+    // configuramos margenes del svg
+    var margins = {
+        top: 10,
+        right: 10,
+        bottom: 20,
+        left: 30
+    };
+    
+    // creamos el SVG
+    var svg = d3
+        .select("body")
+        .append("svg")
+        .attr("width", config.width)
+        .attr("height", config.height);
+
+    // creamos escalas
+    var scaleX = d3.scaleLinear()
+        .domain(d3.extent(data, d => d.horsepower))
+        .range([0 + margins.left, config.width - margins.right]);
+    
+    var scaleY = d3.scaleLinear()
+        .domain(d3.extent(data, d => d.acceleration))
+        .range([config.height - margins.top, 0 + margins.top]);
+    
+    // añadimos los puntos
+    svg
+        .selectAll("body")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("cx", d => scaleX(d.horsepower))
+        .attr("cy", d => scaleY(d.acceleration))
+        .attr("r", "4");
+        
+    // creamos los ejes
+    var xAxis = d3.axisBottom(scaleX);
+    svg.append("g")
+        .attr("transform","translate(0," + (config.height - margins.bottom) + ")")
+        .call(xAxis);
+    
+    var yAxis = d3.axisLeft(scaleY);
+    svg.append("g")
+        .attr("transform","translate(" + (margins.left) + "," + (- margins.top) + ")")
+        .call(yAxis);
+    
+});
